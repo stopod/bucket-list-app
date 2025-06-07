0d547d2b4a4c1b46aa4d2b3fc8f1d60f6f2310ac
stopod-u <gjkv8@icloud.com>
2025-06-08 02:51:37 +0900

    change route

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("instruments", "routes/instruments/instruments.tsx"),
-  route("login", "routes/login.tsx"),
-  route("register", "routes/register.tsx"),
+  route("login", "routes/auth/login.tsx"),
+  route("register", "routes/auth/register.tsx"),
   route("sample", "routes/sample/sample.tsx"),
 ] satisfies RouteConfig;
