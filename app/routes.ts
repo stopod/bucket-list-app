0ebcbeff4489ec2fba68b07ea93a460df5560b87
stopod-u <gjkv8@icloud.com>
2025-06-08 02:34:15 +0900

    edit route

--- app/routes.ts
+++ app/routes.ts
@@ -2,8 +2,8 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("instruments", "routes/instruments.tsx"),
+  route("instruments", "routes/instruments/instruments.tsx"),
   route("login", "routes/login.tsx"),
   route("register", "routes/register.tsx"),
-  route("sample", "routes/sample.tsx"),
+  route("sample", "routes/sample/sample.tsx"),
 ] satisfies RouteConfig;
